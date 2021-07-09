26e2ff2fd926a3b351a78d7c6b9c848cc040f458
조재영 <jdyj@naver.com>
2021-07-09 14:33:15 +0900

    docker config

--- server/src/router/members/index.js
+++ server/src/router/members/index.js
@@ -3,11 +3,6 @@ import * as members from "./members";
 
 const router = new Router();
 
-// router.get("/:id", members.get);
-// router.post("/", members.post, members.responseMiddleware);
-// router.put("/:id", members.put, members.responseMiddleware);
-// router.delete("/:id", members.remove);
-
 router.get("/:email", ...members.read);
 router.post("/", ...members.create);
 router.put("/:email", ...members.update);

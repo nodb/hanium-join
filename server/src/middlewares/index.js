50b83462f62dad84ac11d04a59946af6e147f5b6
futureds <futureds@2uin.com>
2021-07-31 17:40:45 +0900

    feat(member): member 기능 추가
    member 추가, 수정, 삭제, 조회, 상세조회 기능 추가

---
+++ server/src/middlewares/index.js
@@ -0,0 +1,5 @@
+import errorHandleMd from "./errorHandleMd";
+
+export {
+  errorHandleMd,
+};
--- server/src/router/members/index.js
+++ server/src/router/members/index.js
@@ -3,9 +3,19 @@ import * as members from "./members";
 
 const router = new Router();
 
-router.get("/:email", ...members.read);
+// 생성
 router.post("/", ...members.create);
+
+// 리스트 조회
+router.get("/", ...members.readAll);
+
+// 상세 조회
+router.get("/:id", ...members.read);
+
+// 수정
 router.put("/:email", ...members.update);
+
+// 삭제
 router.delete("/:email", ...members.remove);
 
 export default router;

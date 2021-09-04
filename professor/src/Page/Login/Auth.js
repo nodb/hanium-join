48d0702b4b03f99adfd759185a17089ffa9e0fea
EomYoosang <dbtkd1801@gmail.com>
2021-09-04 23:42:31 +0900

    이메일/비밀번호 찾기

--- professor/src/Page/Login/Auth.js
+++
@@ -1,4 +0,0 @@
-const CLIENT_ID = "729cca45e0c408428ac95c62cb111af7";
-const REDIRECT_URI = "http://10.50.48.81:3000/kakao";
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,11 +1,17 @@
 const config = {
   DB_HOST: "localhost",
   DB_USER: "root",
-  DB_PASSWORD: "whwodud123",
+  DB_PASSWORD: "xhRl6034!",
   DB_DATABASE: "mydb",
-  DB_PORT: 13306,
-  DB_CONNECTION_LIMIT: 30,
+  DB_PORT: 3306,
+  DB_CONNECTION_LIMIT: 10,
   JWT_SECRET: "HEURM_JSON_WEB_TOKEN_KEY_!@#$",
+  SENSAPI: {
+    serviceId: "ncp:sms:kr:271301956082:yoosang",
+    serviceSecret: "mfuhfBxNL283yGOy2oYjV1PmadPGOEBg3QEvbiJ4",
+    accessKey: "LsrQNLT0ZklKJqmpYv1W",
+    number: "01045918177",
+  }
 };
 
 export default config;

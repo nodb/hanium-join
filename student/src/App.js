4951c4dbe0379b59d88405c59cb2082d8bf2aa1a
futureds <futureds@2uin.com>
2021-09-05 14:40:59 +0900

    feat(docker): docker 기능 추가
    docker 기능 추가

--- student/src/App.js
+++ student/src/App.js
@@ -22,29 +22,3 @@ const App = () => {
 };
 
 export default App;
-=======
->>>>>>> master
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import ClassLayout from "./ClassLayout";
-import MyPage from "./MyPage";
-import Login from "./Page/Login";
-import Register from "./Register";
-import FindIdPw from "./Page/FindIdPw";
-
-const App = () => {
-  return (
-    <>
-      <Switch>
-        <Route path="/login" exact component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/findIdPassword" exact component={FindIdPw} />
-        <Route path="/student/class" component={ClassLayout} />
-        <Route path="/student/mypage" component={MyPage} />
-        <Redirect to="/login" />
-      </Switch>
-    </>
-  );
-};
-
-export default App;

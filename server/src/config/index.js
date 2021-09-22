74eb52a37928c8db91e0c300610e97a9d35bea0a
마경미 <lottie1255@gmail.com>
2021-09-22 20:17:35 +0900

    폴더 정리

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,7 +3,7 @@ const config = {
   DB_USER: "root",
   DB_PASSWORD: "join1234!@#$",
   DB_DATABASE: "mydb",
-  DB_PORT: 3306,
+  DB_PORT: 13306,
   DB_CONNECTION_LIMIT: 10,
   JWT_SECRET: "HEURM_JSON_WEB_TOKEN_KEY_!@#$",
   SENSAPI: {
@@ -11,7 +11,7 @@ const config = {
     serviceSecret: "mfuhfBxNL283yGOy2oYjV1PmadPGOEBg3QEvbiJ4",
     accessKey: "LsrQNLT0ZklKJqmpYv1W",
     number: "01045918177",
-  }
+  },
 };
 
 export default config;
